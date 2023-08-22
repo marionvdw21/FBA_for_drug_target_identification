dcc47044533329622fd254f99067b9dcf26ea315
marionvdw21 <135644434+marionvdw21@users.noreply.github.com>
2023-08-21 21:35:31 -0400

    Add files via upload

--- make_model/orderExpression.py
+++ make_model/orderExpression.py
@@ -1,6 +1,24 @@
 
 def orderExpression(expressionFile): 
 
+    '''
+    U S A G E : 
+    This script is used to order the expression of a cell line from most expressed genes to least expressed and vice versa. 
+    This is usefull because when performing modification of flux boundaries in a given model, the order 
+    in which is is done matters as some genes share the same reactions. 
+    The priority in terms of modifying flux boundaries can therefore be given to least expressed or more expressed genes, 
+    depending on the expression file used. 
+    Example of usage provided at the end. 
+
+    I N P U T S : 
+    --> expressionFile : a tab-delimited text file containing the gene names (in Bigg ID fomat) as the first colums, 
+    and expression as the second colomn (in log2 read counts). This file can be ouputed from the 'HGNC_to_Recon' script. 
+
+    O U T P U T S : 
+    --> expressionFile_ordered.txt : a txt file which is a copy of the input file exept it is ordered from least expressed gene to most expressed gene
+    --> expressionFile_deordered.txt : a txt file which is a copy of the input file exept it is ordered from most expressed gene to least expressed gene
+    '''
+
     from collections import namedtuple
     from operator import attrgetter
 
@@ -37,5 +55,7 @@ def orderExpression(expressionFile):
 
     return 
 
+
 if __name__ == '__main__': 
-    orderExpression()
+    # example of usage : 
+    orderExpression(ExpressionFile = 'expression_Recon.txt')
